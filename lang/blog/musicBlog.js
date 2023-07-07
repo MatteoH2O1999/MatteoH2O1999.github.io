9c3ff6569bca18b1c9eab183644215b2798e1a49
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2023-07-07 13:18:58 +0200

    Add Hotel Transylvania Musical to live events

--- lang/blog/musicBlog.js
+++ lang/blog/musicBlog.js
@@ -1,10 +1,12 @@
 let musicBlog_en_us = {
     "score": "New score",
+    "live": "New live event",
     "listen": "Listen to it now!"
 }
 
 let musicBlog_it_it = {
     "score": "Nuova colonna sonora",
+    "live": "Nuovo evento dal vivo",
     "listen": "Ascoltalo ora!"
 }
 
